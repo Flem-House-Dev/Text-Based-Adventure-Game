3c2b192a38a3a33586e361983223fd82cc98d332
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-08-10 09:24:27 -0500

    fixed bccrypt import in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const Game = require('../models/Game');
