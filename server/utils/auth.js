d79e638a39fb844c030be5578f4d4dcb86958b63
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-08-12 19:42:00 -0500

    fixed token-sign bug

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,8 @@
 const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretssshhhhhhh';
+// const secret = 'mysecretssshhhhhhh';
+const secret = process.env.JWT_SECRET || 'mysecretssshhhhhhh';
 const expiration = '2h';
 
 module.exports = {
@@ -22,16 +23,21 @@ module.exports = {
     }
 
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
+      const { userId  } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = { _id: userId };
+    } catch (error) {
+      console.log('Invalid token', error.message);
+      throw new GraphQLError('Invalid or expired token', {
+        extensions: {
+          code: 'UNAUTHENTICATED',
+        },
+      });
     }
 
     return req;
   },
-  signToken: function ({ email, username, _id }) {
-    const payload = { email, username, _id };
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  signToken: function ({ _id }) {
+    // const payload = { email, username, _id };
+    return jwt.sign({ userId: _id }, secret, { expiresIn: expiration });
   },
 };
