37e0de0755b9e5700e6ecc70dd3e950b456c3279
Flem-House-Dev <144638343+Flem-House-Dev@users.noreply.github.com>
2024-09-17 22:18:36 -0500

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://flemhousetech:tZJht7c0Xa6xDCIe@cluster0.z4lkeqv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(process.env.MONGODB_URI || '');
 
 module.exports = mongoose.connection;
